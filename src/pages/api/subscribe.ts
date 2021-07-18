99510a56f316279a7f4c49d162b150a05812a440
WalleksMR <walleksmiranda@gmail.com>
2021-07-17 21:30:05 -0300

    listening webHooks on stripe

---
+++ src/pages/api/subscribe.ts
@@ -0,0 +1,64 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { query as q } from 'faunadb'
+import { getSession } from 'next-auth/client'
+import { stripe } from '../../services/stripe'
+import { fauna } from '../../services/fauna'
+
+type User = {
+  ref: {
+    id: string
+  }
+  data: {
+    stripe_customer_id: string
+  }
+}
+
+export default async function Subscribe(
+  request: NextApiRequest,
+  response: NextApiResponse
+) {
+  if (request.method === 'POST') {
+    const session = await getSession({ req: request })
+
+    const user = await fauna.query<User>(
+      q.Get(q.Match(q.Index('user_by_email'), q.Casefold(session.user.email)))
+    )
+    let customerId = user.data.stripe_customer_id
+
+    // Evitar duplicidade de user no stripe
+    if (!customerId) {
+      const stripeCustomer = await stripe.customers.create({
+        email: session.user.email,
+      })
+
+      await fauna.query(
+        q.Update(q.Ref(q.Collection('users'), user.ref.id), {
+          data: { stripe_customer_id: stripeCustomer.id },
+        })
+      )
+
+      customerId = stripeCustomer.id
+    }
+
+    const stripeCheckoutSession = await stripe.checkout.sessions.create({
+      customer: customerId,
+      payment_method_types: ['card'],
+      billing_address_collection: 'required',
+      line_items: [
+        {
+          price: 'price_1JCvwfFv7y1Iex9J2PLuoRdL',
+          quantity: 1,
+        },
+      ],
+      mode: 'subscription',
+      allow_promotion_codes: true,
+      success_url: process.env.STRIPE_SUCCESS_URL,
+      cancel_url: process.env.STRIPE_CANCEL_URL,
+    })
+
+    return response.status(200).json({ sessionId: stripeCheckoutSession.id })
+  } else {
+    response.setHeader('Allow', 'POST')
+    response.status(405).end('Method not allowed.')
+  }
+}
